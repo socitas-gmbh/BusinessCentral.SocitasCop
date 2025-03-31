da30c33c50a1c16fa5d454bb4a0d7f54161b9fca
CarstenMertes <165934295+Carsten-Mertes@users.noreply.github.com>
2025-03-31 08:28:08 +0000

    Added Rule 0002 LockTimeout

--- rules/Rule0001VariableNamingConventionShouldNotStartWithlorg.cs
+++ rules/Rule0001VariableNamingConventionShouldNotStartWithlorg.cs
@@ -8,25 +8,25 @@ namespace SocitasCodeCop;
 [DiagnosticAnalyzer]
 public class Rule0001VariableNaming : DiagnosticAnalyzer
 {
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = 
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
         ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0001VariableNameShouldNotStartWithLorG);
 
     public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(new Action<SyntaxNodeAnalysisContext>(this.AnalyzeLocalVariableNaming), SyntaxKind.VariableDeclaration);
 
     private void AnalyzeLocalVariableNaming(SyntaxNodeAnalysisContext ctx)
-    {                
+    {
         // Check if object is pending or obsolete        
         if (ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePendingMove || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) {
             return;
         }
-                
-        VariableDeclarationSyntax syntax = ctx.Node as VariableDeclarationSyntax;        
+
+        VariableDeclarationSyntax syntax = ctx.Node as VariableDeclarationSyntax;
         if (syntax != null)
-        {            
+        {
             string variableName = syntax.GetNameStringValue();
             if (variableName.StartsWith('l') || variableName.StartsWith('g'))
             {
-                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0001VariableNameShouldNotStartWithLorG, syntax.Name.GetLocation(), syntax.Name));                
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0001VariableNameShouldNotStartWithLorG, syntax.Name.GetLocation(), syntax.Name));
             }
         }
     }
@@ -36,10 +36,10 @@ public class Rule0001VariableNaming : DiagnosticAnalyzer
         public static readonly DiagnosticDescriptor Rule0001VariableNameShouldNotStartWithLorG = new(
             id: "SO0001",
             title: "l or g Variable",
-            messageFormat: "No variable name should start with l or g",
+            messageFormat: "No variable name should start with l or g.",
             category: "Design",
             defaultSeverity: DiagnosticSeverity.Warning, isEnabledByDefault: true,
-            description: "No variable name should start with l or g",
+            description: "No variable name should start with l or g.",
             helpLinkUri: "https://some.url/SO0001");
     }
 }
