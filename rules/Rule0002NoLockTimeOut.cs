5f1d2c448ae87291add755eefd5fc29164c94d49
CarstenMertes <165934295+Carsten-Mertes@users.noreply.github.com>
2025-04-11 16:17:39 +0000

    Renamed instances of 'SocitasCodeCop' to 'SocitasCop'

--- rules/Rule0002NoLockTimeOut.cs
+++ rules/Rule0002NoLockTimeOut.cs
@@ -3,7 +3,7 @@ using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
 
-namespace SocitasCodeCop;
+namespace SocitasCop;
 
 [DiagnosticAnalyzer]
 public class Rule0002NoLockTimeOut : DiagnosticAnalyzer
